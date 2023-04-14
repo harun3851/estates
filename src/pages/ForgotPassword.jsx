08e6dfb47dac3bc985334448cd80e4ccd004a56b
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-04-14 06:58:51 +0300

    complete forget email functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 export default function ForgotPassword() {
 
@@ -10,7 +12,17 @@ export default function ForgotPassword() {
   function onChange(e) {
    setEmail(e.target.value);
   }
+async function onSubmit(e){
+  e.preventDefault()
+  try {
+    const auth = getAuth()
+    await sendPasswordResetEmail(auth,email);
+    toast.success("email was sent");
+  } catch (error) {
+    toast.error("could not send reset password")
+  }
 
+}
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password </h1>
@@ -23,7 +35,7 @@ export default function ForgotPassword() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border-gray-300 rounded transition easy-in-out"
               type="email"
