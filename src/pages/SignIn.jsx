f56ff6cf100aeeb773da978d745b2e5b27f397dc
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-04-14 06:37:43 +0300

    complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
-
+import { signInWithEmailAndPassword, auth, getAuth  } from "firebase/auth";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
@@ -10,12 +12,25 @@ export default function SignIn() {
     password: "",
   });
   const { email, password } = formData;
-
+const navigate = useNavigate();
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  }
+  async function onSubmit(e){
+    e.preventDefault()
+    try{
+const auth = getAuth();
+const userCredential = await signInWithEmailAndPassword(auth,email, password)
+if(userCredential.user){
+  navigate("/")
+}
+    }catch (error){
+      toast.error("Bad user credentials")
+    }
+
   }
 
   return (
@@ -30,7 +45,7 @@ export default function SignIn() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border-gray-300 rounded transition easy-in-out"
               type="email"
