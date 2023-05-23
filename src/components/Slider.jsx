d12deea53de98f5004972083d181e856fa6aacbc
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-05-23 12:12:07 +0300

    slider component created

---
+++ src/components/Slider.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react'
+
+export default function Slider() {
+
+  return (
+    <div>Slider</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Slider from "../components/Slider";
+import { Timestamp, collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+import Spinner from "../components/Spinner";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [listings, setListing]=useState(null);
+  const [loading, setLoading] =useState(true);
+  useEffect(()=>{
+
+    async function fetchListings(){
+      const listingsRef = collection(db, "listings")
+      const q = query(listingsRef, orderBy("timestamp", "desc"),limit(5));
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id:doc.id,
+          data:doc.data(),
+        }); 
+      });
+      setListing(listings);
+      setLoading(false)
+    }
+    fetchListings()
+        
+  },[]);
+  if(loading){
+    return <Spinner />
+  }
+  if(listings.length===0){
+    return<></>;
+  }
+  return <div>
+    <Slider />
+  </div>;
 }
