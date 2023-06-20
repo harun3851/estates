bfa6369a2d0bb2122282ba2fe27916ffe78edde4
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-06-20 18:29:27 +0300

    removing scrollbar from home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,6 +38,7 @@ export default function Home() {
           });
         });
         setOfferListings(listings);
+        console.log(listings);
       } catch (error) {
         console.log(error);
       }
